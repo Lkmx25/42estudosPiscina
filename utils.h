9a26cd75b0c41f7fbed60a0d52cea36c5c21fdf6
Lkmx25 <lcsknj25@gmail.com>
2026-02-03 21:36:43 -0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -7,7 +7,7 @@ int		ft_is_digit(char c);
 int		ft_strlen(char *str);
 int		ft_is_printable(char c);
 int		compare_char(char a, char b, char c);
-void	swap_pointer(int *curr, int *prev);
+void	swap_pointer(int **curr, int **prev);
 int	ft_min(int a, int b, int c);
 
 
